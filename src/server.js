62e29c8b6425ec66b019a602c0cde92b7cc62df3
Enzo Duarte <enzoduarte2019@gmail.com>
2025-05-16 23:21:14 -0300

    instalacion de socket.io y correccion de ruta de server.js a app.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const { Server } = require('socket.io');
-const app = require('./src/app');
+const app = require('./app');
 
 const server = http.createServer(app);
 const io = new Server(server);
